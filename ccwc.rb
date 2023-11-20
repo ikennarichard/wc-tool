97c5ed767b24b0b8fb7500d908d81273b49ef11a
Ikenna Richard <oguejioforrichard@gmail.com>
2023-11-20 13:11:03 +0100

    Add options parser config

--- ccwc.rb
+++ ccwc.rb
@@ -1,2 +1,27 @@
 #!/usr/bin/env ruby
 
+require 'optparse'
+require_relative 'utils'
+
+options = {}
+
+OptionParser.new do |opts|
+  opts.banner = "Usage: ./ccwc.rb [OPTION]... [FILE]..."
+
+  opts.on("-c", "--bytes", "print the byte count") do |c|
+    options[:bytes] = c
+  end
+end.parse!
+
+file_path = ARGV.first
+
+if file_path.nil?
+  puts 'File path is missing. Usage: ./ccwc.rb [OPTION] [FILE]'
+  exit 1
+end
+
+if options[:bytes]
+  CountOptions.count_bytes(file_path)
+else
+  puts 'Please specify a valid option (-c)'
+end
