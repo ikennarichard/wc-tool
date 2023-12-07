ce96eaa48edec1395c7b28a337f0d0f4b21317dc
Ikenna Richard <oguejioforrichard@gmail.com>
2023-12-07 13:06:59 +0100

    Add method for counting chars, words

--- utils.rb
+++ utils.rb
@@ -10,4 +10,33 @@ module CountOptions
     line_count = File.foreach(file_path).count
     puts "#{line_count} #{file_path}"
   end
+
+  # print word counts
+  def self.count_words(file_path)
+    word_count = File.read(file_path).split(/\s+/)
+    puts "#{word_count.size} #{file_path}"
+  end
+
+  # print character counts
+  def self.count_chars(file_path)
+    chars_count = File.read(file_path).bytesize
+    puts "#{chars_count} #{file_path}"
+  end
+
+  # print character counts
+  def self.print_all(file_path)
+    line_count = File.foreach(file_path).count
+    word_count = File.read(file_path).split(/\s+/).size
+    byte_count = File.size(file_path)
+    print "#{line_count} #{word_count} #{byte_count} #{file_path}"
+  end
+
+  # read from stdin if theres no filename
+  def self.print_input_lines_count(input)
+    line_count = 0
+    input.each_line do
+      line_count += 1
+    end
+    puts line_count
+  end
 end
